74517ede096c268f49c745d54259afb50e91a4cf
unknown <you@example.com>
2020-10-21 00:07:07 -0500

    Entrega de proyecto

---
+++ JS/scrip.js
@@ -0,0 +1,8 @@
+$("li").mouseover(function(){
+    $(this).css("font-size", "20px").css("background", "pink").css
+    })
+ 
+ $("li").mouseout(function(){
+    $(this).css("font-size", "25px").css("background", "green").css
+ })
+ 
